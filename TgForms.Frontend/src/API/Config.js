4fb785b3d592a528018633fa2ab926b5fe0eee8b
Otabek Rustamov <otabek.pro@hotmail.com>
2023-10-09 18:47:39 +0500

    Configs upd

--- TgForms.Frontend/src/API/Config.js
+++ TgForms.Frontend/src/API/Config.js
@@ -1,4 +1,5 @@
 export default class Config {
-  static BaseUrl ="https://9836-37-110-215-121.ngrok-free.app"
+  static BaseUrl ="https://tgforms-bot.space"
+  //static BaseUrl ="https://9836-37-110-215-121.ngrok-free.app"
   // static BaseUrl ="http://localhost:59312"
 }
